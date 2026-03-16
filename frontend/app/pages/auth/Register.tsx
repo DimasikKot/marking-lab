bfdcbdd102869ed4a1014a9436517317563ecba5
gbven <titovivan02@mail.ru>
2026-03-16 14:20:10 +0500

    feat: страница регистрации

--- frontend/app/pages/auth/Register.tsx
+++ frontend/app/pages/auth/Register.tsx
@@ -1,7 +1,60 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export function Register() {
+
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async () => {
+
+    const response = await fetch(
+      import.meta.env.VITE_BACKEND_URL + "/auth/user/",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password
+        })
+      }
+    );
+
+    if (response.ok) {
+      alert("Пользователь создан");
+      navigate("/login");
+    } else {
+      alert("Ошибка регистрации");
+    }
+  };
+
   return (
     <div>
-      <p>Регистрация</p>
+
+      <h1>Регистрация</h1>
+
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Пароль"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleRegister}>
+        Зарегистрироваться
+      </button>
+
     </div>
-  )
+  );
 }
