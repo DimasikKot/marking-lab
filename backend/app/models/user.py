82fd6c85b2f490fb669cc3b92b33f4942a421f6e
Кожухарев Дмитрий <dima.kozhuharew@yandex.ru>
2026-03-24 01:02:29 +0500

    feat: добавил роли

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -6,7 +6,9 @@ from sqlalchemy.sql import func
 class User(AuthBase):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    created_at = Column(TIMESTAMP, server_default=func.now())
+    id              = Column(Integer, primary_key=True, index=True)
+    role            = Column(String(255), default='user')  # 'user', 'moderator', 'admin'
+    username        = Column(String(255), unique=True, nullable=False, index=True)
+    email           = Column(String(255), unique=True, nullable=False, index=True)
+    hashed_password = Column(String(255), nullable=False)
+    created_at      = Column(TIMESTAMP, server_default=func.now())
--- marking-lab-auth.sql
+++ marking-lab-auth.sql
@@ -1,7 +1,9 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    email VARCHAR NOT NULL UNIQUE,
-    hashed_password VARCHAR NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id              SERIAL          PRIMARY KEY,
+    role            VARCHAR(255)    DEFAULT 'user',     -- 'user', 'moderator', 'admin'
+    username        VARCHAR(255)    NOT NULL UNIQUE,
+    email           VARCHAR(255)    NOT NULL UNIQUE,
+    hashed_password VARCHAR(255)    NOT NULL,
+    created_at      TIMESTAMP       DEFAULT CURRENT_TIMESTAMP
 );
