f68188667621d70ec3497f48d5aae38ff3653a47
Кожухарев Дмитрий <dima.kozhuharew@yandex.ru>
2026-03-24 22:15:45 +0500

    feat: сделал подписи к методам пользователей

--- backend/app/services/user.py
+++ backend/app/services/user.py
@@ -5,18 +5,22 @@ from jose import jwt
 from datetime import datetime, timedelta, timezone
 from app.core.config import settings
 
+
 password_crypt_context: CryptContext = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def get_password_hash(password: str) -> str:
+    """Хеширует пароль с помощью bcrypt и возвращает хешированную строку"""
     return password_crypt_context.hash(password)
 
 
 def verify_password(plain_password, hashed_password) -> bool:
+    """Проверяет, соответствует ли открытый пароль хешу пароля"""
     return password_crypt_context.verify(plain_password, hashed_password)
 
 
 def create_user(db: Session, username: str, email: str, password: str) -> User:
+    """Создаёт нового пользователя с хешированным паролем и сохраняет его в базе данных"""
     hashed_password: str = get_password_hash(password)
     user: User = User(username=username, email=email, hashed_password=hashed_password)
     db.add(user)
@@ -26,6 +30,7 @@ def create_user(db: Session, username: str, email: str, password: str) -> User:
 
 
 def authenticate_user(db: Session, email: str, password: str) -> User | None:
+    """Проверяет email и пароль, возвращает пользователя, если аутентификация успешна, иначе None"""
     user: User = db.query(User).filter(User.email == email).first()
 
     # Ни в коем случае не возвращаем в чем именно проблема, возвращаем ошибку и всё
@@ -41,6 +46,7 @@ def authenticate_user(db: Session, email: str, password: str) -> User | None:
 
 
 def encode_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
+    """Создаёт JWT-токен с данными из словаря и временем истечения, если указано"""
     # Копируем, чтобы не изменять передаваемый словарь
     # Данные в словаре будут зашифрованы, но их можно прочитать зная секретный ключ из параметров среды
     to_encode: dict = data.copy()
@@ -55,9 +61,7 @@ def encode_access_token(data: dict, expires_delta: timedelta | None = None) -> s
     return jwt.encode(to_encode, settings.JWT_ACCESS_TOKEN_SECRET, algorithm="HS256")
 
 def decode_access_token(token: str) -> dict[str, any] | None:
-    """
-    Возвращает payload из JWT-токена или None, если токен недействителен/просрочен
-    """
+    """Возвращает payload из JWT-токена или None, если токен недействителен/просрочен"""
     try:
         payload = jwt.decode(
             token,
@@ -76,6 +80,7 @@ def decode_access_token(token: str) -> dict[str, any] | None:
         return None
 
 def print_access_token_data(token: str) -> None:
+    """Печатает данные из токена, если он действителен, иначе сообщает, что токен недействителен/просрочен"""
     data = decode_access_token(token)
 
     if data:
