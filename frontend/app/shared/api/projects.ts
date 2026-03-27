9b86584f9a76ad86acb3c1450dcc3b96500bafeb
wakakue <accerue00@gmail.com>
2026-03-27 11:44:53 +0300

    запросы к бэкенду

---
+++ frontend/app/shared/api/projects.ts
@@ -0,0 +1,89 @@
+import api from "./axios";
+import axios from "axios";
+
+interface Project {
+  id: number;
+  name: string;
+  is_public: boolean;
+  created_at: string;
+  updated_at: string;
+}
+
+interface GetProjectsResponse {
+  data: Project[];
+}
+
+interface PostProjectRequest {
+  name: string;
+  is_public: boolean;
+}
+
+interface DeleteProjectResponce {
+  detail: string;
+}
+
+export const fetchProjects = async (): Promise<
+  GetProjectsResponse | undefined
+> => {
+  try {
+    const response = await api.get<GetProjectsResponse>("/projects");
+    return response.data;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      console.error("Ошибка при запросе:", error.message);
+    }
+  }
+};
+
+export const fetchProjectById = async (
+  id: string | number,
+): Promise<Project | undefined> => {
+  try {
+    const response = await api.get<Project>(`/projects/${id}`);
+    return response.data;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      console.error("Ошибка при запросе:", error.message);
+    }
+  }
+};
+
+export const createProject = async (
+  data: PostProjectRequest,
+): Promise<Project | undefined> => {
+  try {
+    const response = await api.post<Project>("/projects", data);
+    return response.data;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      console.error("Ошибка при запросе:", error.message);
+    }
+  }
+};
+
+export const patchProjectById = async (
+  id: string | number,
+  data: PostProjectRequest,
+): Promise<Project | undefined> => {
+  try {
+    const response = await api.patch<Project>(`/projects${id}`, data);
+    return response.data;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      console.error("Ошибка при запросе:", error.message);
+    }
+  }
+};
+
+export const deleteProjectById = async (
+  id: string | number,
+): Promise<DeleteProjectResponce | undefined> => {
+  try {
+    const response = await api.delete<DeleteProjectResponce>(`/projects${id}`);
+    return response.data;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      console.error("Ошибка при запросе:", error.message);
+    }
+  }
+};
