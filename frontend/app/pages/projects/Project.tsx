81908aca94aee6019e812a230b4c328f19bfa547
Titov_Ivan <titovivan02@mail.ru>
2026-04-14 23:12:38 +0500

    feat: Начал делать Top bar

--- frontend/app/pages/projects/Project.tsx
+++ frontend/app/pages/projects/Project.tsx
@@ -1,13 +1,35 @@
 import { useNavigate, useParams } from "react-router-dom";
-
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 import { Button } from "@/shared/components/Button";
-
+import { Text } from "@/shared/components/Text";
 export function Project() {
   const navigate = useNavigate();
   const { projectId } = useParams();
 
   return (
-    <div>
+    //Логика такая: Первый <Tab> соотносится с первым <TabPanel>
+    <Tabs>
+    <TabList>
+      <Tab>
+        <Text variant="normal">
+          Перейти в файлы
+        </Text>
+      </Tab>
+      <Tab>
+        <Text variant="normal">
+          Перейти в эксперименты
+        </Text>
+      </Tab>
+      <Tab>
+        <Text variant="normal">
+          Перейти в модели
+        </Text>
+      </Tab>
+    </TabList>
+
+    <TabPanel>
+      <h2><div>
       <p>Страница проекта</p>
       <Button
         onClick={() => {
@@ -17,6 +39,14 @@ export function Project() {
       >
         Перейти в файлы проекта
       </Button>
-    </div>
-  );
+    </div></h2>
+    </TabPanel>
+    <TabPanel>
+      <h2>Any content 2</h2>
+    </TabPanel>
+  </Tabs>
+  
+);
+
+
 }
