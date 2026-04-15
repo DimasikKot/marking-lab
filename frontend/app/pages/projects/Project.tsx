cfda580df32d3bbcc348c2e15c8913451f19f0d1
Titov_Ivan <titovivan02@mail.ru>
2026-04-15 10:10:53 +0500

    feat:сделал TopBar в Projects

--- frontend/app/pages/projects/Project.tsx
+++ frontend/app/pages/projects/Project.tsx
@@ -1,52 +1,42 @@
-import { useNavigate, useParams } from "react-router-dom";
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
-import { Button } from "@/shared/components/Button";
 import { Text } from "@/shared/components/Text";
+import { Files } from './Files';
+import { Experiments } from './Experiments';
+import { Models } from './Models';
+
 export function Project() {
-  const navigate = useNavigate();
-  const { projectId } = useParams();
-
   return (
-    //Логика такая: Первый <Tab> соотносится с первым <TabPanel>
     <Tabs>
-    <TabList>
-      <Tab>
-        <Text variant="normal">
-          Перейти в файлы
-        </Text>
-      </Tab>
-      <Tab>
-        <Text variant="normal">
-          Перейти в эксперименты
-        </Text>
-      </Tab>
-      <Tab>
-        <Text variant="normal">
-          Перейти в модели
-        </Text>
-      </Tab>
-    </TabList>
+      <TabList>
+        <Tab>
+          <Text variant="normal">
+            Перейти в файлы
+          </Text>
+        </Tab>
+        <Tab>
+          <Text variant="normal">
+            Перейти в эксперименты
+          </Text>
+        </Tab>
+        <Tab>
+          <Text variant="normal">
+            Перейти в модели
+          </Text>
+        </Tab>
+      </TabList>
 
-    <TabPanel>
-      <h2><div>
-      <p>Страница проекта</p>
-      <Button
-        onClick={() => {
-          navigate(`/projects/${projectId}/files`);
-        }}
-        variant="primary"
-      >
-        Перейти в файлы проекта
-      </Button>
-    </div></h2>
-    </TabPanel>
-    <TabPanel>
-      <h2>Any content 2</h2>
-    </TabPanel>
-  </Tabs>
-  
-);
+      <TabPanel>
+        <Files />
+      </TabPanel>
 
+      <TabPanel>
+        <Experiments />
+      </TabPanel>
 
+      <TabPanel>
+        <Models />
+      </TabPanel>
+    </Tabs>
+  );
 }
