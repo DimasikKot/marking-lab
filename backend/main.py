6db3f67328c5ef25374ebca988ca3a63bd3f7bb0
Кожухарев Дмитрий <dima.kozhuharew@yandex.ru>
2026-04-27 13:03:52 +0500

    lint: забыл закоммитить

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,7 @@ app: FastAPI = FastAPI()
 origins: list[str] = [
     "http://localhost:5173",  # Фронтенд на Vite
     "http://127.0.0.1:5173",  # Альтернативный локальный хост
+    "*",
 ]
 
 # Добавляем CORS Middleware
